bddfc17fe36d92a9317372a3217b19e49c10a667
Kai Cilliers <kaijohan428@gmail.com>
2020-09-18 16:54:55 +0100

    testing smack lib

--- app/src/main/java/com/example/jabbertest/MainActivity.kt
+++ app/src/main/java/com/example/jabbertest/MainActivity.kt
@@ -8,21 +8,22 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.jivesoftware.smack.AbstractXMPPConnection
+import org.jivesoftware.smack.SmackConfiguration
 import org.jivesoftware.smack.StanzaListener
 import org.jivesoftware.smack.chat2.Chat
 import org.jivesoftware.smack.chat2.ChatManager
-import org.jivesoftware.smack.filter.StanzaFilter
+import org.jivesoftware.smack.filter.AndFilter
+import org.jivesoftware.smack.filter.FromMatchesFilter
 import org.jivesoftware.smack.filter.StanzaTypeFilter
+import org.jivesoftware.smack.packet.Message
 import org.jivesoftware.smack.packet.Presence
+import org.jivesoftware.smack.packet.Stanza
 import org.jivesoftware.smack.roster.Roster
-import org.jivesoftware.smack.roster.RosterEntry
-import org.jivesoftware.smack.roster.RosterListener
 import org.jivesoftware.smack.tcp.XMPPTCPConnection
-import org.jivesoftware.smackx.muc.MultiUserChatManager
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration
+import org.jivesoftware.smackx.jiveproperties.JivePropertiesManager
 import org.jxmpp.jid.EntityBareJid
 import org.jxmpp.jid.impl.JidCreate
-import org.jxmpp.jid.parts.Resourcepart
-import org.jxmpp.jid.util.JidUtil
 import timber.log.Timber
 
 
@@ -32,7 +33,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
-
     }
 
     private lateinit var _connection: AbstractXMPPConnection
@@ -54,9 +54,23 @@ class MainActivity : AppCompatActivity() {
         Timber.v("Configuring conenction")
         // Create a connection and login to the example.org XMPP service.
         // Create a connection and login to the example.org XMPP service.
-        val connection: AbstractXMPPConnection =
-            XMPPTCPConnection("android", "1234", "pimux.de")
+
+        // Connection builder
+        val config = XMPPTCPConnectionConfiguration.builder()
+            .setUsernameAndPassword("android", "1234")
+            .setXmppDomain("pimux.de")
+            .setHost("pimux.de")
+            .setPort(5222)
+            .setResource("MobileAndroid")
+            .build()
+        val connection = XMPPTCPConnection(config)
         connection.connect().login()
+        Roster.setDefaultSubscriptionMode(Roster.SubscriptionMode.accept_all)
+
+        // Simple Connection
+//        val connection: AbstractXMPPConnection =
+//            XMPPTCPConnection("android", "1234", "pimux.de")
+//        connection.connect().login()
 
         if (!chatMade) {
             val chatManager = ChatManager.getInstanceFor(connection)
@@ -71,8 +85,8 @@ class MainActivity : AppCompatActivity() {
 
     fun sendClick(v: View) {
         Timber.v("clicked")
+        stanzalistener()
         val msg = etMutiInput.text.toString()
-
         chat.send(msg)
     }
 
@@ -80,6 +94,7 @@ class MainActivity : AppCompatActivity() {
         chatManager.addIncomingListener { from, message, chat ->
             System.out.println("New message from " + from + ": " + message.getBody())
             updateUI(from.toString(), message.body)
+            echoIncomingMessage(from, message, chat)
         }
         Timber.d("Listener made...")
     }
@@ -103,6 +118,24 @@ class MainActivity : AppCompatActivity() {
         val presence = Presence(Presence.Type.available)
         presence.status = "I am here"
         _connection.sendStanza(presence)
+    }
+
+    fun echoIncomingMessage(from: EntityBareJid, msg: Message, chat: Chat) {
+        // Send back the same text the other user sent us
+        chat.send("you told me ${msg.body}")
+    }
+
+    fun stanzalistener() {
+        Timber.d("Stanza Listener...")
+        val filter = AndFilter(
+            StanzaTypeFilter.MESSAGE,
+            FromMatchesFilter.create(JidCreate.entityBareFrom("myphone@pimux.de"))
+        )
+        val stanzaCollector = _connection.createStanzaCollector(filter)
+        val listen = StanzaListener {
+            println("$it")
+        }
+        _connection.addAsyncStanzaListener(listen, filter)
     }
 
 }
