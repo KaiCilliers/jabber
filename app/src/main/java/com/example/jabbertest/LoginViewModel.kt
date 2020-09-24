d18a6b5be492e1a2fcc4bdcfa31379da46ab63d3
Kai Cilliers <kaijohan428@gmail.com>
2020-09-24 12:05:59 +0100

    login activity added with fragment

---
+++ app/src/main/java/com/example/jabbertest/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.jabbertest
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    val variableA: String = "var_A"
+    val variableB: String = "var_B"
+}
