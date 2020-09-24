0e1881bd8a3a852ee2198753ef5852a336a63cda
Kai Cilliers <kaijohan428@gmail.com>
2020-09-24 19:27:56 +0100

    radiobutton group clicks events for changed values

--- app/src/main/java/com/example/jabbertest/LoginFragment.kt
+++ app/src/main/java/com/example/jabbertest/LoginFragment.kt
@@ -1,14 +1,18 @@
 package com.example.jabbertest
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.jabbertest.databinding.FragLoginBinding
+import kotlinx.android.synthetic.main.frag_login.*
 import timber.log.Timber
+
 
 class LoginFragment : Fragment() {
 
@@ -40,5 +44,21 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         Timber.d(SharedPref.getString("data", "No Data :("))
+        rb_group_accounts.setOnCheckedChangeListener{ group, checkedId ->
+            when(checkedId) {
+                rb_one.id -> Timber.d("Changed Radiobutton value - $checkedId")
+                rb_two.id -> Timber.d("Changed Radiobutton value - $checkedId")
+                rb_three.id -> Timber.d("Changed Radiobutton value - $checkedId")
+                rb_four.id -> Timber.d("Changed Radiobutton value - $checkedId")
+                rb_five.id -> Timber.d("Changed Radiobutton value - $checkedId")
+                rb_six.id -> Timber.d("Changed Radiobutton value - $checkedId")
+                rb_seven.id -> Timber.d("Changed Radiobutton value - $checkedId")
+                rb_eight.id -> Timber.d("Changed Radiobutton value - $checkedId")
+            }
+        }
+    }
+
+    fun accountSelected(v: View) {
+        Timber.d("The radio button that was clicked is - ${v.id}")
     }
 }
--- app/src/main/java/com/example/jabbertest/LoginViewModel.kt
+++ app/src/main/java/com/example/jabbertest/LoginViewModel.kt
@@ -3,21 +3,32 @@ package com.example.jabbertest
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import timber.log.Timber
 
 class LoginViewModel : ViewModel() {
     val variableA: String = "var_A"
     val variableB: String = "var_B"
     val variableNext: String = "NEXT"
 
+    //Data
+    private val _account = MutableLiveData<String>()
+    val account: LiveData<String>
+        get() = _account
+
     // Navigation
     private val _navToChats = MutableLiveData<Boolean>()
     val navToChats: LiveData<Boolean>
         get() = _navToChats
     fun navToChats() {
-        // TODO TIMBER LOGS
+        Timber.d("Navigating to chats...")
         _navToChats.value = true
     }
     fun onNavigatedToChats() {
         _navToChats.value = false
+    }
+
+    // Other functions
+    fun captureAccount() {
+
     }
 }
