ec37b83d293bd817ad7831f4d3501bc0b0e004f1
Kai Cilliers <kaijohan428@gmail.com>
2020-09-24 12:19:35 +0100

    data binding to login activity

--- app/src/main/java/com/example/jabbertest/LoginFragment.kt
+++ app/src/main/java/com/example/jabbertest/LoginFragment.kt
@@ -5,13 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.jabbertest.databinding.FragLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var viewModel: LoginViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.frag_login, container, false)
+        val binding = FragLoginBinding.inflate(inflater)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        binding.viewModel = viewModel
+        return binding.root
     }
 }
